44a938e56fc9b80632027940df62c759d6fb885b
izaizaiza <117947026+izaizaiza@users.noreply.github.com>
2023-12-07 17:13:24 -0700

    removed search button, added hover color

--- app/features/search-bar.js
+++ app/features/search-bar.js
@@ -17,27 +17,17 @@ export default function SearchBar({ query,  handleSearch, handleSubmit}) {
                     w-3/4'
                     >
             <p className='text-lg pt-5 pb-2 pl-5 pr-5 text-custom-neon-blue text-center'>
-                Search for art pieces from the Chicago Art Museum and Harvard Art Museum</p>
+                Search for art from the Chicago Art Museum and Harvard Art Museum</p>
             <form 
             onSubmit={handleSubmit}
             className='flex p-5 mb-5 w-full justify-center'>
                 <input
                 id = "searchBar"
-                className='text-custom-black-pearl w-3/4 rounded p-1 mr-5' 
+                className='text-custom-black-pearl w-3/4 rounded p-1 mr-5 border-4 hover:border-custom-neon-blue' 
                 type="text"
                 placeholder="Search for art pieces..." 
                 value={query} 
                 onChange={handleSearch} />
-                {/*<button 
-                type="submit"
-                className=' bg-white
-                            text-custom-black-pearl  
-                            hover:bg-custom-blue-light
-                            hover:text-custom-pearl
-                            font-bold
-                            p-2
-                            rounded'
-                >Search</button>*/}
             </form>
         </div>
     )
